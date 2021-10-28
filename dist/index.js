e2f0263654b3519d58358061bd3e0ea941f9d44f
Alyssa DeMetro <leanndemetro@gmail.com>
2021-10-28 14:13:47 -0400

    test action

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+
+async function Action() {
+  console.log('Hello World')
+}
+
+Action()
+
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
